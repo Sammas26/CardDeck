040f0ce0533d1fecce96314e42ce438df0f52f30
sajogre <skater25sam@gmail.com>
2025-02-05 12:39:48 -0600

    Init Push

--- CardDeck/CardDeck.cpp
+++ CardDeck/CardDeck.cpp
@@ -3,10 +3,32 @@
 
 using namespace std;
 
+enum CardSuit {CS_CLUB, CS_DIAMOND, CS_HEART, CS_SPADE};
+
+enum CardRank {CR_ACE, CR_TWO, CR_THREE, CR_FOUR, CR_FIVE, CR_SIX, CR_SEVEN, CR_EIGHT, CR_NINE, CR_TEN, CR_JACK, CR_QUEEN, CR_KING};
+
+
+class Card {
+public:
+	Card(CardSuit s = CS_CLUB, CardRank r = CR_ACE);
+	string getStr() const;
+
+private:
+	static string toSuitStr(CardSuit s);
+	static string toRankStr(CardRank r);
+};
+
+Card::Card(CardSuit s, CardRank r) {
+	suit = s; rank = r;
+}
+
+string Card::getStr() const {
+	return toRankStr(rank) + " of " + toSuitStr(suit);
+}
 
 
 
 int main()
 {
-  
+	
 }
