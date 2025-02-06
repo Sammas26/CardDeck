711ed1e5771eb2d6cce6b52bbdd6be7cdae03eb1
sajogre <skater25sam@gmail.com>
2025-02-06 10:57:08 -0600

    Added toSuitStr and toRankStr

--- CardDeck/CardDeck.cpp
+++ CardDeck/CardDeck.cpp
@@ -26,6 +26,79 @@ string Card::getStr() const {
 	return toRankStr(rank) + " of " + toSuitStr(suit);
 }
 
+string Card::toSuitStr(CardSuit s) {
+	string ans;
+
+	switch (s) {
+		case CS_CLUB:
+			ans = "Club";
+			break;
+		case CS_DIAMOND:
+			ans = "Diamond";
+			break;
+		case CS_HEART:
+			ans = "Heart";
+			break;
+		case CS_SPADE:
+			ans = "Spade";
+			break;
+
+	}
+
+	return ans;
+
+}
+
+string Card::toRankStr(CardRank r) {
+	string ans;
+
+	switch (r) {
+		case CR_ACE:
+			ans = "Ace";
+			break;
+		case CR_KING:
+			ans = "King";
+			break;
+		case CR_QUEEN:
+			ans = "Queen";
+			break;
+		case CR_JACK:
+			ans = "Jack";
+			break;
+		case CR_TEN:
+			ans = "Ten";
+			break;
+		case CR_NINE:
+			ans = "Nine";
+			break;
+		case CR_EIGHT:
+			ans = "Eight";
+			break;
+		case CR_SEVEN:
+			ans = "Seven";
+			break;
+		case CR_SIX:
+			ans = "Six";
+			break;
+		case CR_FIVE:
+			ans = "Five";
+			break;
+		case CR_FOUR:
+			ans = "Four";
+			break;
+		case CR_THREE:
+			ans = "Three";
+			break;
+		case CR_TWO:
+			ans = "Two";
+			break;
+
+	}
+
+	return ans;
+
+}
+
 
 
 int main()
